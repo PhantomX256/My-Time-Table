d56f1f10eca12e769da882b5bc2197e29aeead86
PhantomX256 <vedverma2004@gmail.com>
2024-07-15 21:53:55 +0530

    Modified the way time table data is read

---
+++ data.js
@@ -0,0 +1,79 @@
+// Contains the lectures in day order one
+export const dayOrderOne = [
+        {
+                title: 'Computer Networks',  // 1st Lecture
+                time: '12:30 - 14:15'
+        },
+        {
+                title: 'Machine Learning',  // 2nd Lecture
+                time: '14:20 - 16:00'
+        },
+        {
+                title: 'Open Elective',  // 3rd Lecture
+                time: '16:00 - 17:00'
+        }
+];
+
+// Contains lectures in day order two
+export const dayOrderTwo = [
+        {
+                title: 'Professional Elective',  // 1st Lecture
+                time: '8:00 - 9:40'
+        },
+        {
+                title: 'Open Elective',  // 2nd Lecture
+                time: '9:45 - 11:30'
+        },
+        {
+                title: 'Computer Networks',  // 3rd Lecture
+                time: '11:35 - 12:25'
+        }
+];
+
+// Contains lectures in day order three
+export const dayOrderThree = [
+        {
+                title: 'Discrete Mathematics',  // 1st Lecture
+                time: '12:30 - 14:15'
+        },
+        {
+                title: 'Computer Networks',  // 2nd Lecture
+                time: '14:20 - 15:10'
+        },
+        {
+                title: 'Formal Language and Automata',  // 3rd Lecture
+                time: '16:00 - 16:50'
+        }
+];
+
+// Contains lectures in day order four
+export const dayOrderFour = [
+        {
+                title: 'Formal Language and Automata',  // 1st Lecture
+                time: '8:00 - 9:40'
+        },
+        {
+                title: 'Cloud Native AI Deployment',  // 2nd Lecture
+                time: '9:45 - 10:35'
+        },
+        {
+                title: 'Discrete Mathematics',  // 3rd Lecture
+                time: '11:35 - 12:25'
+        }
+];
+
+// Contains lectures in day order four
+export const dayOrderFive = [
+        {
+                title: 'Discrete Mathematics',  // 1st Lecture
+                time: '14:20 - 15:10'
+        },
+        {
+                title: 'Machine Learning',  // 2nd Lecture
+                time: '15:10 - 16:00'
+        },
+        {
+                title: 'Formal Language and Automata',  // 3rd Lecture
+                time: '16:00 - 16:50'
+        }
+];
---
+++ render.js
@@ -0,0 +1,36 @@
+import * as dayOrders from "./data.js";
+
+// Function to create a sub-div for each lecture
+function createLectureDiv(lecture) {
+    // Create the main lecture div
+    const $lectureDiv = $("<div></div>").addClass(
+        "bg-[#A78295] rounded-xl p-4 shadow-[#331D2C] shadow-md transition-all hover:scale-[1.02] max-md:hover:scale-[1.05] text-center max-md:p-2"
+    );
+
+    // Create the subject span
+    const $subjectSpan = $("<span></span>")
+        .addClass("subject block text-lg max-md:text-[17px]")
+        .text(lecture.title);
+
+    // Create the time span
+    const $timeSpan = $("<span></span>")
+        .addClass("time max-md:text-[15px]")
+        .text(lecture.time);
+
+    // Append the spans to the lecture div
+    $lectureDiv.append($subjectSpan);
+    $lectureDiv.append($timeSpan);
+
+    return $lectureDiv;
+}
+
+// Iterate over each dayOrder in the dayOrders object
+Object.keys(dayOrders).forEach((dayOrderKey, index) => {
+    const dayOrder = dayOrders[dayOrderKey];
+    const dayOrderDiv = $(`#day-${index + 1}`);
+
+    dayOrder.forEach((lecture) => {
+        const $lectureDiv = createLectureDiv(lecture);
+        dayOrderDiv.append($lectureDiv);
+    });
+});
